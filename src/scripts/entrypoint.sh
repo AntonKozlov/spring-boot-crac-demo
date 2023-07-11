7b53523e2d154eed3821019a779c455e37315bf0
Anton Kozlov <akozlov@azul.com>
2023-07-11 18:26:00 +0300

    Unprivileged checkpoint

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -4,7 +4,8 @@ CRAC_FILES_DIR=`eval echo ${CRAC_FILES_DIR}`
 mkdir -p $CRAC_FILES_DIR
 
 if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
-  echo 128 > /proc/sys/kernel/ns_last_pid; java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar&
+  ( echo 128 > /proc/sys/kernel/ns_last_pid ) 2>/dev/null || while [ $(cat /proc/sys/kernel/ns_last_pid) -lt 128 ]; do :; done
+  java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar&
   sleep 5
   jcmd /opt/app/spring-boot-crac-demo-1.0.0-SNAPSHOT.jar JDK.checkpoint
   sleep infinity
